88cf8cbec125029e996d4f5e4530aa64d89c2589
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-09-06 22:23:56 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stukdo::Application.config.secret_key_base = '3ff465d3014c6db2b6301741ee49f4ef73d3258b892b6d616a12e8017e1725917d3104a4c60b164bd91caccf6f76c197023bd591919263e9c169d3e143177376'
