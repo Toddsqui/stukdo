9100fee422943b4d65c476bd0dd8bf447502bafb
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-09-14 22:33:37 +0900

    Added link detection

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912124602) do
+ActiveRecord::Schema.define(version: 20150914132648) do
 
   create_table "tasks", force: true do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "state",      default: "To_do"
+    t.string   "state",        default: "To_do"
+    t.text     "content_html"
   end
 
   create_table "users", force: true do |t|
